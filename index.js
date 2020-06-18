e00fe92526952135693715b80da65288239067c1
revyrob <kayle.robson@gmail.com>
2020-06-18 21:00:04 +0200

    add pages

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/map', (req, res) => res.render('qgis/map'))
+  .get('/about', (req, res) => res.render('pages/index2-about'))
+  .get('/how', (req, res) => res.render('pages/how'))
     // .get('/map', (req, res) => res.sendFile(path.join(__dirname + '/views/qgis/map.html')))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
